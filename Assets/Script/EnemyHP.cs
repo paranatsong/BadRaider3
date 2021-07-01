49cbf4c01994cebe4ac8cda9e6feb7741b64e612
DESKTOP-AF9IGPV\pondf <paranat.song@bumail.net>
2021-07-02 01:22:00 +0700

    Second Update

---
+++ Assets/Script/EnemyHP.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHP : MonoBehaviour
+{
+    public int enemyHP;
+    private int currentHP;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = enemyHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentHP <= 0)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+    }
+}
