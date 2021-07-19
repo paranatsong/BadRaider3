debd68e0500f98ee689d3586e882f70fa6c96f78
DESKTOP-AF9IGPV\pondf <paranat.song@bumail.net>
2021-07-20 01:35:50 +0700

    Halloween Update

---
+++ Assets/Script/Fade.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    public Image DialogueBox;
+    public Text  text;
+    void Start()
+    {
+        DialogueBox.canvasRenderer.SetAlpha(1.0f);
+        text.canvasRenderer.SetAlpha(1.0f);
+        fadeout();
+    }
+
+    void fadeout()
+    {
+        DialogueBox.CrossFadeAlpha(0, 5, false);  
+        text.CrossFadeAlpha(0, 5, false);
+    }
+}
